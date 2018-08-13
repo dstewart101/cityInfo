8c2238c5734b4ffa925f540826dad3aac9a6d5fb
David Stewart <david.stewart@communities-ni.gov.uk>
2018-08-13 14:17:20 +0100

    MVC Services and middleware added

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -16,6 +16,7 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,12 +33,8 @@ namespace CityInfo.API
                 app.UseExceptionHandler();
             }
 
-            
+            app.UseMvc();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
