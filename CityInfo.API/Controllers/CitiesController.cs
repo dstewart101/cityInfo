f70da52f591fe712609db4f5e2e366d9c71d1b65
David Stewart <david.stewart@communities-ni.gov.uk>
2018-08-29 15:51:06 +0100

    get cities with CitiesDTO

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController
+    {
+        [HttpGet()]
+        public JsonResult GetCities() {
+            return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+    }
+}
---
+++ CityInfo.API/Data/CitiesDataStore.cs
@@ -0,0 +1,24 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Data
+{
+    public class CitiesDataStore
+    {
+        public List<CityDTO> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore() {
+            Cities = new List<CityDTO>()
+            {
+                new CityDTO() { Id = 1, Name = "New York", Description = "The big Apple" },
+                new CityDTO() { Id = 2, Name = "Belfast", Description = "Home town" },
+                new CityDTO() { Id = 3, Name = "Ballygowan", Description = "Legends are born here"}
+            };
+        }
+    }
+}
