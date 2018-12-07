57810d008917923b02fee229404b44d3944b5268
David Stewart <david.stewart@communities-ni.gov.uk>
2018-12-07 11:14:14 +0000

    add mail service with env configuation

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
@@ -16,6 +17,14 @@ namespace CityInfo.API
 {
     public class Startup
     {
+
+        public static IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -51,14 +60,13 @@ namespace CityInfo.API
                 app.UseDeveloperExceptionPage();
             }
 
-            else {
+            else
+            {
                 app.UseExceptionHandler();
             }
 
-
             app.UseStatusCodePages();
             app.UseMvc();
-
         }
     }
 }
